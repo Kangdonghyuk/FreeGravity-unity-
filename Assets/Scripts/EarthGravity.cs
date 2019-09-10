5450df97f066ba0fdfc67f3598b8e747675808ab
Liunx <kdh950812@gmail.com>
2019-09-10 23:11:35 +0900

    free gravity manage game test

---
+++ Assets/Scripts/EarthGravity.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthGravity : MonoBehaviour
+{
+    public Transform sun;
+
+    Vector3 gravity;
+    float gravityPower;
+    float distance;
+
+    void Start()
+    {
+        gravityPower = 50;
+        distance = 0f;
+    }
+
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, sun.position);
+
+        if(distance >= gravityPower)
+            distance = gravityPower;
+
+        gravity.x = (transform.position.x - sun.position.x) /
+            Mathf.Abs((distance / (distance - gravityPower)));
+        gravity.y = (transform.position.y - sun.position.y) /
+            Mathf.Abs((distance / (distance - gravityPower)));
+
+            //gravity = -gravity / gravityPower;
+
+        transform.position = transform.position + -gravity * Time.deltaTime;
+    }
+}
