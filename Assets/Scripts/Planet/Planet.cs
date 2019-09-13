d76e3418d2059de0076e91801d742d68efe460c5
Liunx <kdh950812@gmail.com>
2019-09-13 21:13:21 +0900

    코드 최적화

--- Assets/Scripts/Planet/Planet.cs
+++ Assets/Scripts/Planet/Planet.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    public float mass;
+    public bool isGravity;
+    public float mass = 1;
+    public float G = 1; // gravity constant number, generally set 1
+
+    Rigidbody2D rigid;
+    float forceValue;
+    Vector2 forceDirection;
+    public Vector2 forceStart; 
+
+    void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+    }
 
     void Start()
     {
-        
+        rigid.gravityScale = 0;
+        rigid.mass = mass;
+
+        rigid.AddForce(forceStart * 1000);
     }
 
     void Update()
     {
         
+    }
+
+    void FixedUpdate() {
+        if(isGravity)
+            Gravity();
+    }
+
+    void Gravity() {
+        float distance;
+
+        for(int index = 0; index < PlanetMNG.I.planetList.Length; index++) {
+            if(PlanetMNG.I.planetList[index].gameObject == gameObject)
+                continue;
+            distance = Vector2.Distance(PlanetMNG.I.planetList[index].transform.position, transform.position);
+            forceValue = G * (PlanetMNG.I.planetList[index].mass * mass) / (distance*distance);
+            forceDirection = (PlanetMNG.I.planetList[index].transform.position - transform.position).normalized;
+
+            rigid.AddForce(forceDirection * forceValue);
+        }
     }
 }
